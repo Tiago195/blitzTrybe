04d355acb2ae3ac1e80ac050d8741b34fcebdf66
Tiago195 <tiagocf475@gmail.com>
2022-07-03 15:01:30 -0300

    fix: add inteface param error

--- middlewares/errorGeneric.ts
+++ middlewares/errorGeneric.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
+import IError from '../interfaces/error.interface'
 
-const errorGeneric = (error, _req: Request, res: Response, _next: NextFunction) => {
+const errorGeneric = (error: IError, _req: Request, res: Response, _next: NextFunction) => {
   if(error.statusCode) return res.status(error.statusCode).json({message: error.message})
 
   res.status(500).json({message: 'Internal server error'})
