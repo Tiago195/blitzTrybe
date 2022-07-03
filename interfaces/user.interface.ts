aaa3ebefd52e63cacd6857d10c7e46095581a9d6
Tiago195 <tiagocf475@gmail.com>
2022-07-03 19:01:08 -0300

    feat: implements type newUser

--- interfaces/user.interface.ts
+++ interfaces/user.interface.ts
@@ -1,11 +1,13 @@
 export default interface IUser {
   id: number,
-  password: number,
+  password: string,
   name: string,
   email: string,
   github?: string,
   is_admin: number
 }
+
+export type newUser = Omit<IUser, 'id'>;
 
 // export interface INewUser extends IUser {
 //   id: number,
